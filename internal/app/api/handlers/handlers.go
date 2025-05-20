96ef96e5f86a87dea8455990c987c913f4d71afa
Pavel Denisov <p.denisov@vk.team>
2025-05-20 14:26:29 +0300

    Iteration 8 implementation.

--- internal/app/api/handlers/handlers.go
+++ internal/app/api/handlers/handlers.go
@@ -19,9 +19,9 @@ func NewHandlers(a *api.API) *handlers {
 
 func (h *handlers) ConfigureRouter() {
 	// Хэндлеры первого спринта
-	h.api.Router.Handle("/", middleware.WithLogging(http.HandlerFunc(h.ShortenerHandler), h.api.Logger))
-	h.api.Router.Handle("/{id}", middleware.WithLogging(http.HandlerFunc(h.UnShortenerHandler), h.api.Logger))
+	h.api.Router.Handle("/", middleware.WithLogging(middleware.WithCompress(http.HandlerFunc(h.ShortenerHandler)), h.api.Logger))
+	h.api.Router.Handle("/{id}", middleware.WithLogging(middleware.WithCompress(http.HandlerFunc(h.UnShortenerHandler)), h.api.Logger))
 
 	// Хэндлеры второго спринта
-	h.api.Router.Handle("/api/shorten", middleware.WithLogging(http.HandlerFunc(h.ShortenHandler), h.api.Logger))
+	h.api.Router.Handle("/api/shorten", middleware.WithLogging(middleware.WithCompress(http.HandlerFunc(h.ShortenHandler)), h.api.Logger))
 }
