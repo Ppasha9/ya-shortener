cdfd54eaa1cbfeefa4946049138a2839655021db
Pavel Denisov <p.denisov@vk.team>
2025-06-17 11:51:34 +0300

    Iteration 10: register new ping handler.

--- internal/app/api/handlers/handlers.go
+++ internal/app/api/handlers/handlers.go
@@ -24,4 +24,7 @@ func (h *handlers) ConfigureRouter() {
 
 	// Хэндлеры второго спринта
 	h.api.Router.Handle("/api/shorten", middleware.WithLogging(middleware.WithCompress(http.HandlerFunc(h.ShortenHandler)), h.api.Logger))
+
+	// Хэндлеры третьего спринта
+	h.api.Router.Handle("/ping", middleware.WithLogging(http.HandlerFunc(h.PingHandler), h.api.Logger))
 }
